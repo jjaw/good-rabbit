07e9661ba84b64a4d0cb273178e86b146d27b951
Will Jaw <willjaw@wills-mbp.lan>
2023-06-30 21:25:53 -0700

    changed how to handle resend api key on vercel

--- src/app/openai/api/route.js
+++ src/app/openai/api/route.js
@@ -2,8 +2,14 @@ import { NextResponse } from 'next/server';
 import EmailTemplate from "@/app/components/email-template";
 import { Resend } from "resend";
 import { query } from "@/app/components/senti-analysis";
+import { RESEND_API_KEY } from '$env/static/public';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// on vercel
+const resend = new Resend(RESEND_API_KEY);
+
+// works for non vercel
+// 
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 const generateMessage = async ({
   senderName,
