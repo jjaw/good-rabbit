61027a1c775f439e1a36b4ac90db1ab612f489be
Will Jaw <willjaw@wills-mbp.lan>
2023-07-01 01:00:41 -0700

    organized file structure.

--- src/app/openai/api/route.js
+++ src/app/openai/api/route.js
@@ -5,41 +5,7 @@ import { query } from "@/app/components/senti-analysis";
 import generateMessage from "@/app/components/generate-message"
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-/*
-const generateMessage = async ({
-  recipientName,
-  extra,
-}) => {
-  try {
-    const response = await fetch(
-      "https://api.openai.com/v1/completions",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-        },
-        body: JSON.stringify({
-          model: "text-davinci-003",
-          prompt: `Write a nice message (no more than 88 words) of positivity for ${recipientName}. I am a friend but that's irrelevant. Some relevant information are ${extra}.`,
-          max_tokens: 120,
-          temperature: 1,
-          top_p: 0.77,
-        }),
-      }
-    );
-    
-    const GPTdata = await response.json();
-    
-    
-    return GPTdata.choices[0].text;
 
-  } catch (err) {
-    console.error(err);
-    throw err;
-  }
-} 
-*/
 export async function POST(req) {
   try {
     const body = await req.json();
