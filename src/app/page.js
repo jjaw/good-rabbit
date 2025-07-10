03639e04bad63a1a38c81c21999d37f7da79435a
Will Jaw <willjaw@wills-mbp.lan>
2025-07-09 22:15:23 -0700

    changed bgcolor and form width.

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Form from './components/main-form';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Send positive thoughts to your friends..&nbsp;
           <code className="font-mono font-bold">why wait?</code>
@@ -29,9 +29,12 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="sm:w-6/12 md:w-6/12 lg:w-6/12">
-        <Form />
+      <div className="flex justify-center w-full">
+        <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
+          <Form />
+        </div>
       </div>
+
     </main>
   )
 }
